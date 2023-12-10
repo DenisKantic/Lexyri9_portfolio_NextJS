315a1899c2f1099778213e1e55e53b3964303f32
DenisKantic <denis.kantic18@gmail.com>
2023-12-10 20:14:35 +0100

    created main component and added 'fixed' option in navbar

---
+++ app/components/Main.jsx
@@ -0,0 +1,7 @@
+export default function Main(){
+    return (
+        <div className="h-[100vh]">
+            <h1 className="text-white">Test</h1>
+        </div>
+    )
+}
--- app/page.js
+++ app/page.js
@@ -1,9 +1,12 @@
 import Navigation from "./components/Navigation"
+import Main from "./components/Main"
 
 export default function Home() {
   return (
    <div className="bg-[#000D2E] min-h-[100vh]">
     <Navigation />
+    <Main />
+    <Main />
    </div>
   )
 }
