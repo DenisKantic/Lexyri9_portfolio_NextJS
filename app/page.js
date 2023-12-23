dc376e55865e5af33b3e3cf0a6d4b9001dd4dece
DenisKantic <denis.kantic18@gmail.com>
2023-12-23 19:38:47 +0100

    about me component template finished

--- app/page.js
+++ app/page.js
@@ -1,11 +1,13 @@
 import Navigation from "./components/Navigation"
 import Main from "./components/Main"
+import AboutMe from "./components/AboutMe"
 
 export default function Home() {
   return (
    <div className="bg-[#000D2E] min-h-[100vh]">
     <Navigation />
     <Main />
+    <AboutMe />
    </div>
   )
 }
