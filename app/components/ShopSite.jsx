b1ed5b6cabb0990e8d558595c2e82e5b0c41aba3
DenisKantic <denis.kantic18@gmail.com>
2023-12-25 21:37:47 +0100

    added functionality for changing images according to the color

--- app/components/ShopSite.jsx
+++ app/components/ShopSite.jsx
@@ -1,6 +1,43 @@
+"use client"
 import Image from "next/image"
+import { useState } from "react";
 
-export default function ShopSite(){
+
+    {/* importing all hoodies color*/}
+
+    const white = require('../../public/images/MOCKUP/Hoodie/white.png');
+    const blue = require('../../public/images/MOCKUP/Hoodie/blue.png');
+    const green = require('../../public/images/MOCKUP/Hoodie/green.png');
+    const red = require('../../public/images/MOCKUP/Hoodie/red.png');
+    const grey = require('../../public/images/MOCKUP/Hoodie/grey.png');
+    const hodies = {white,blue,green,red,grey};
+
+    {/* importing all t-shirts */ }
+    const blackT = require('../../public/images/MOCKUP/T-shirt/BLACK.png');
+    const greenT = require('../../public/images/MOCKUP/T-shirt/GREEN.png');
+    const blueT = require('../../public/images/MOCKUP/T-shirt/NAVY.png');
+    const redT = require('../../public/images/MOCKUP/T-shirt/RED.png');
+    const whiteT = require('../../public/images/MOCKUP/T-shirt/WHITE.png');
+    const tShirts = {blackT,greenT,blueT,redT,whiteT}
+
+    {/* importing all caps */}
+    const greenCap = require('../../public/images/MOCKUP/Cap/CAPGREEN.png')
+    const blackCap = require('../../public/images/MOCKUP/Cap/CAPBLACK.png')
+    const redCap = require('../../public/images/MOCKUP/Cap/CAPRED.png')
+    const whiteCap = require('../../public/images/MOCKUP/Cap/CAPWHITE.png')
+    const blueCap = require('../../public/images/MOCKUP/Cap/CAPBLUE.png')
+    const navyCap = require('../../public/images/MOCKUP/Cap/MILITARYCAP.png')
+    const caps = {greenCap,blackCap,redCap, whiteCap, blueCap, navyCap};
+    
+    
+    
+    export default function ShopSite(){
+
+        const [selected,setSelected] = useState(hodies.grey);
+        const [selectedT,setSelectedT] = useState(tShirts.whiteT);
+        const [selectedCap, setSelectedCap] = useState(caps.blackCap)
+
+        
     return (
         <div className="w-[80%] mx-auto min-h-[100vh]">
 
@@ -12,7 +49,7 @@ export default function ShopSite(){
             <div className="text-white w-[70%] h-[50vh] flex flex-row items-center"> {/* card item parent */}
 
                 <div className="w-[45%] flex justify-center items-center"> {/* left side image */}
-                <Image src="/images/MOCKUP/Hoodie/grey.png" unoptimized
+                <Image src={selected} unoptimized alt="hoddie"
                 width={100} height={100} className="w-auto h-auto flex justify-center items-center"/>
                 </div>
 
@@ -26,10 +63,14 @@ export default function ShopSite(){
                         </select>
                         <br />
                         <p className="pt-6">Color</p>
-                        <select name="" id="" className="w-full p-2 h-[50px] bg-transparent border-[1px] rounded-md border-white text-white outline-none cursor-pointer">
-                            <option value="" className="h-[50px]">Front</option>
-                            <option value="">Back</option>
-                        </select>
+                        <div className="items-center justify-around">
+                            <button className="h-[40px] w-[80px] bg-blue-400 text-black" onClick={()=>setSelected(hodies.blue)}>Blue</button>
+                            <button className="h-[40px] w-[80px] bg-red-500 text-black" onClick={()=>setSelected(hodies.red)}>Red</button>
+                            <button className="h-[40px] w-[80px] bg-green-500 text-black" onClick={()=>setSelected(hodies.green)}>Green</button>
+                            <button className="h-[40px] w-[80px] bg-gray-500 text-black" onClick={()=>setSelected(hodies.grey)}>Gray</button>
+                            <button className="h-[40px] w-[80px] bg-white text-black" onClick={()=>setSelected(hodies.white)}>White</button>
+
+                        </div>
 
                         <div className="flex flex-row justify-center place-items-center pt-10">
 
@@ -71,10 +112,14 @@ export default function ShopSite(){
 
                     <div>
                         <p className="pt-6 pb-2">Color</p>
-                        <select name="" id="" className="w-full p-2 h-[50px] bg-transparent border-[1px] rounded-md border-white text-white outline-none cursor-pointer">
-                            <option value="" className="h-[50px]">Front</option>
-                            <option value="">Back</option>
-                        </select>
+                        <div className="items-center justify-around">
+                            <button className="h-[40px] w-[80px] bg-blue-400 text-black" onClick={()=>setSelected(hodies.blue)}>Blue</button>
+                            <button className="h-[40px] w-[80px] bg-red-500 text-black" onClick={()=>setSelected(hodies.red)}>Red</button>
+                            <button className="h-[40px] w-[80px] bg-green-500 text-black" onClick={()=>setSelected(hodies.green)}>Green</button>
+                            <button className="h-[40px] w-[80px] bg-gray-500 text-black" onClick={()=>setSelected(hodies.grey)}>Gray</button>
+                            <button className="h-[40px] w-[80px] bg-white text-black" onClick={()=>setSelected(hodies.white)}>White</button>
+
+                        </div>
 
                         <div className="flex flex-row justify-center place-items-center pt-10">
 
@@ -108,7 +153,7 @@ export default function ShopSite(){
             <div className="text-white w-[70%] h-[50vh] flex flex-row items-center"> {/* card item parent */}
 
                 <div className="w-[45%] flex justify-center items-center"> {/* left side image */}
-                <Image src="/images/MOCKUP/T-shirt/WHITE.png" unoptimized
+                <Image src={selectedT} unoptimized
                 width={100} height={100} className="w-auto h-auto flex justify-center items-center"/>
                 </div>
 
@@ -116,10 +161,14 @@ export default function ShopSite(){
 
                     <div>
                         <p className="pt-6 pb-2">Color</p>
-                        <select name="" id="" className="w-full p-2 h-[50px] bg-transparent border-[1px] rounded-md border-white text-white outline-none cursor-pointer">
-                            <option value="" className="h-[50px]">Front</option>
-                            <option value="">Back</option>
-                        </select>
+                        <div className="items-center justify-around">
+                            <button className="h-[40px] w-[80px] bg-blue-400 text-black" onClick={()=>setSelectedT(tShirts.blackT)}>Black</button>
+                            <button className="h-[40px] w-[80px] bg-red-500 text-black" onClick={()=>setSelectedT(tShirts.redT)}>Red</button>
+                            <button className="h-[40px] w-[80px] bg-green-500 text-black" onClick={()=>setSelectedT(tShirts.greenT)}>Green</button>
+                            <button className="h-[40px] w-[80px] bg-gray-500 text-black" onClick={()=>setSelectedT(tShirts.blueT)}>Blue</button>
+                            <button className="h-[40px] w-[80px] bg-white text-black" onClick={()=>setSelectedT(tShirts.whiteT)}>White</button>
+
+                        </div>
 
                         <div className="flex flex-row justify-center place-items-center pt-10">
 
@@ -159,7 +208,7 @@ export default function ShopSite(){
             <div className="text-white w-[70%] h-[50vh] flex flex-row items-center"> {/* card item parent */}
 
                 <div className="w-[45%] flex justify-center items-center"> {/* left side image */}
-                <Image src="/images/MOCKUP/Cap/CAP_BLACK.png" unoptimized
+                <Image src={selectedCap} unoptimized
                 width={100} height={100} className="w-auto h-auto flex justify-center items-center"/>
                 </div>
 
@@ -167,10 +216,16 @@ export default function ShopSite(){
 
                     <div>
                         <p className="pt-6 pb-2">Color</p>
-                        <select name="" id="" className="w-full p-2 h-[50px] bg-transparent border-[1px] rounded-md border-white text-white outline-none cursor-pointer">
-                            <option value="" className="h-[50px]">Front</option>
-                            <option value="">Back</option>
-                        </select>
+                        <div className="items-center justify-around">
+                            <button className="h-[40px] w-[80px] bg-blue-400 text-black" onClick={()=>setSelectedCap(caps.blueCap)}>Blue</button>
+                            <button className="h-[40px] w-[80px] bg-red-500 text-black" onClick={()=>setSelectedCap(caps.redCap)}>Red</button>
+                            <button className="h-[40px] w-[80px] bg-green-500 text-black" onClick={()=>setSelectedCap(caps.greenCap)}>Green</button>
+                            <button className="h-[40px] w-[80px] bg-gray-500 text-black" onClick={()=>setSelectedCap(caps.whiteCap)}>White</button>
+                            <button className="h-[40px] w-[80px] bg-white text-black" onClick={()=>setSelectedCap(caps.navyCap)}>Military</button>
+                            <button className="h-[40px] w-[80px] bg-white text-black" onClick={()=>setSelectedCap(caps.blackCap)}>Black</button>
+
+
+                        </div>
 
                         <div className="flex flex-row justify-center place-items-center pt-10">
 
@@ -206,5 +261,5 @@ export default function ShopSite(){
 
 
         </div>
-    )
-}
+        )
+    }
--- app/shop/page.js
+++ app/shop/page.js
@@ -1,3 +1,4 @@
+'use client'
 import ShopSite from "../components/ShopSite"
 import Navigation from "../components/Navigation"
 
