569f9db0392cc2820faf047ce8eaae988aae6aa8
DenisKantic <denis.kantic18@gmail.com>
2023-12-29 18:42:20 +0100

    navbar responsive finished

--- app/components/Navigation.jsx
+++ app/components/Navigation.jsx
@@ -16,41 +16,50 @@ export default function Navigation() {
 
 
         <div>
-         <button className="bg-white rounded-xl px-5 py-3 flex items-center justify-center text-xl text-black
-                           xxs:hidden md:flex">
+         <a className="bg-white rounded-xl px-5 py-3 flex items-center justify-center text-black
+                           xxs:hidden 
+                           md:flex md:px-3 md:py-1 md:text-sm md:text-xl
+                           lg:px-5 lg:py-3 lg:text-xl"
+                           href="https://www.paypal.com/paypalme/lexyri"
+                           target="_blank" rel="noopener noreferrer">
             <TbCoffee className="mr-2" size={25}/>
             Buy me a coffee
-            </button>
+            </a>
         </div> 
 
         <ul className='xxs:hidden md:flex justify-start items-center text-white'>
             <Image src="/images/logo.webp" alt="lexiry_logo" width={50} height={50} 
             className="mr-10"/>
-            <li className="pr-10 hover:cursor-pointer transition-all duration-200 ease-in-out group">
+            <li className="hover:cursor-pointer transition-all duration-200 ease-in-out group
+                           md:pr-2 lg:pr-10">
                <span className="p-2 bg-left-bottom bg-gradient-to-r from-pink-500 to-pink-500 bg-[length:0%_2px] 
                bg-no-repeat group-hover:bg-[length:100%_2px] transition-all duration-500 ease-out">
                 Home
                </span>
             </li>
-            <li className="pr-10 hover:cursor-pointer transition-all duration-200 ease-in-out group">
+            <li className="hover:cursor-pointer transition-all duration-200 ease-in-out group
+                           md:pr-2 lg:pr-10">
                <span className="p-2 bg-left-bottom bg-gradient-to-r from-pink-500 to-pink-500 bg-[length:0%_2px] 
                bg-no-repeat group-hover:bg-[length:100%_2px] transition-all duration-500 ease-out">
                 About me
                </span>
             </li>
-            <li className="pr-10 hover:cursor-pointer transition-all duration-200 ease-in-out group">
+            <li className="hover:cursor-pointer transition-all duration-200 ease-in-out group
+                           md:pr-2 lg:pr-10">
                <span className="p-2 bg-left-bottom bg-gradient-to-r from-pink-500 to-pink-500 bg-[length:0%_2px] 
                bg-no-repeat group-hover:bg-[length:100%_2px] transition-all duration-500 ease-out">
                 Shop
                </span>
             </li>
-            <li className="pr-10 hover:cursor-pointer transition-all duration-200 ease-in-out group">
+            <li className="hover:cursor-pointer transition-all duration-200 ease-in-out group
+                           md:pr-2 lg:pr-10">
                <span className="p-2 bg-left-bottom bg-gradient-to-r from-pink-500 to-pink-500 bg-[length:0%_2px] 
                bg-no-repeat group-hover:bg-[length:100%_2px] transition-all duration-500 ease-out">
                 Contact
                </span>
             </li>
-            <li className="pr-10 hover:cursor-pointer transition-all duration-200 ease-in-out group">
+            <li className="hover:cursor-pointer transition-all duration-200 ease-in-out group
+                           md:pr-2 lg:pr-10">
 
             <h1 className='mr-3'>Language:</h1>
             </li>
