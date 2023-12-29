b19535356822449cd922682722ced09445b377a5
DenisKantic <denis.kantic18@gmail.com>
2023-12-29 12:53:46 +0100

    template for navbar responsive is finished, needs some tweaks

--- app/components/Navigation.jsx
+++ app/components/Navigation.jsx
@@ -1,20 +1,29 @@
+"use client";
 import { TbCoffee } from "react-icons/tb";
+import {AiOutlineMenu, AiOutlineClose, AiOutlineHome, AiOutlineInfoCircle, AiOutlineShoppingCart, AiOutlinePhone} from 'react-icons/ai'
 import Image from "next/image";
+import React, { useState } from "react";
 
 
 export default function Navigation() {
+
+   const [nav,setNav] = useState(false);
+
     return (
      <div className="w-[90%] mx-auto">
-      <div className="flex justify-between items-center flex-row-reverse h-[8vh] pt-5 fixed w-[90%]"> 
+      <div className="flex h-[8vh] pt-5 fixed w-[90%] 
+                     xxs:items-start xxs:justify-start md:flex-row-reverse md:justify-between md:items-center">
+
+
         <div>
-         <button className="bg-white rounded-xl px-5 py-3 flex items-center justify-center text-xl text-black">
+         <button className="bg-white rounded-xl px-5 py-3 flex items-center justify-center text-xl text-black
+                           xxs:hidden md:flex">
             <TbCoffee className="mr-2" size={25}/>
             Buy me a coffee
             </button>
         </div> 
 
-        <div>
-         <ul className="flex justify-center items-center text-white">
+        <ul className='xxs:hidden md:flex justify-start items-center text-white'>
             <Image src="/images/logo.webp" alt="lexiry_logo" width={50} height={50} 
             className="mr-10"/>
             <li className="pr-10 hover:cursor-pointer transition-all duration-200 ease-in-out group">
@@ -46,7 +55,27 @@ export default function Navigation() {
             <h1 className='mr-3'>Language:</h1>
             </li>
          </ul>
+   
+         <div className="md:hidden xxs:flex">
+            <AiOutlineMenu className='mr-2 cursor-pointer md:hidden text-white flex' size={30} onClick={()=> setNav(!nav)}></AiOutlineMenu>
+            <h1 className="text-2xl text-white">Lexyri9</h1>
+            <div className={ nav ? 'bg-black/80 w-full fixed h-screen z-10 top-0 left-0 duration-200' : 'flex'}>
+            <div className={nav ? 'flex justify-center items-center text-white' : 'fixed left-[-100%] w-0 top-0 duration-300 bg-purple-400'}>
+            <AiOutlineClose className='absolute top-4 right-4 cursor-pointer' 
+                        onClick={()=> setNav(!nav)}  size={30}></AiOutlineClose> 
 
+
+               <ul className={ nav ? 'flex flex-col items-start justify-center p-5 text-black text-lg mt-[50px] bg-white text-black w-full h-[40vh]' : 'hidden'}>
+                  <li className='mr-2 flex items-center py-2'><AiOutlineHome size={22} className='mr-5'/><a href="#home" onClick={()=> setNav(!nav)}>HOME</a></li>
+                  <li className='mr-2 flex items-center py-2'><AiOutlineInfoCircle size={22} className='mr-5' /><a href="#aboutUs" onClick={()=> setNav(!nav)}>ABOUT ME</a></li>
+                  <li className='mr-2 flex items-center py-2'><AiOutlineShoppingCart size={22} className='mr-5' /><a href="#products" onClick={()=> setNav(!nav)}>SHOP</a></li>
+                  <li className='mr-2 flex items-center py-2'><AiOutlinePhone  size={22} className='mr-5'/><a href="#contact" onClick={()=> setNav(!nav)}>CONTACT</a></li>
+               </ul>
+
+         </div>
+
+
+        </div>
         </div>
 
       </div>        
