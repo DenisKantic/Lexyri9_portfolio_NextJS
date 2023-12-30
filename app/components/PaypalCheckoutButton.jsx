b1138a82da67b1eb2dee589818bee026fcab9cc7
DenisKantic <denis.kantic18@gmail.com>
2023-12-30 22:40:16 +0100

    bootstrap dropdown with many changes

---
+++ app/components/PaypalCheckoutButton.jsx
@@ -0,0 +1,70 @@
+import React, {useEffect, useState} from 'react'
+import { PayPalButtons } from '@paypal/react-paypal-js'
+
+const PaypalCheckoutButton = ({product}) => {
+
+    const { description,price } = product; // destructure props 
+
+    const [paidFor, setPaidFor] = useState(false);
+    const [error, setError] = useState(false)
+
+
+
+    const handleApprove = (orderID) =>{
+        // call backend to fulfill order
+        
+        //if response is success
+        setPaidFor(true);
+    
+        //refresh user's account or subscription status
+
+
+        //if the response is error 
+        // alert the user 
+    }
+
+    if(paidFor){
+        // display success message, modal or redirect the user to the success page
+        alert("Thank you for your purchase")
+    }
+
+    if(error){
+        //display error message 
+        alert(error)
+    }
+
+  return (
+    <PayPalButtons 
+    forceReRender={[description,price]}
+        createOrder={(data,actions)=>{
+            return actions.order.create({
+                purchase_units: [
+                    { 
+                        description,   // ovdje nastaje problem, on ne cita uopste nekada sta je za description
+                        amount: {
+                            value: price // ovdje uredno cita cijenu 
+                        }
+                    }
+                ]
+    
+            })
+        }}
+
+        onApprove={async (data,actions) =>{
+            const order = await actions.order.capture();
+            console.log("order",order)
+
+            handleApprove(data.orderID)
+        }}
+
+        onError={(err)=>{
+            setError(err);
+            console.error("Paypal checkout on error", err);
+        }}
+    >
+
+    </PayPalButtons>
+  )
+}
+
+export default PaypalCheckoutButton
--- app/components/ShopSite.jsx
+++ app/components/ShopSite.jsx
@@ -1,6 +1,11 @@
 "use client"
 import Image from "next/image"
 import { useState } from "react";
+import PaypalCheckoutButton from "./PaypalCheckoutButton";
+import { NavItem, PageItem } from "react-bootstrap";
+import Dropdown from 'react-bootstrap/Dropdown';
+
+
 
 
     {/* importing all hoodies color*/}
@@ -37,6 +42,17 @@ import { useState } from "react";
         const [selectedT,setSelectedT] = useState(tShirts.whiteT);
         const [selectedCap, setSelectedCap] = useState(caps.blackCap)
 
+        const [hodie,setHodie] = useState("Gray");
+
+        const [price,setPrice] = useState(20);
+        const [size,setSize] = useState("S");
+        const [number,setNumber] = useState(2);
+        const [position,setPosition] = useState("Front");
+
+        console.log(hodie);
+        console.log(size);
+        console.log(number);
+        console.log(price);
         
     return (
         <div className="w-[80%] mx-auto min-h-[100vh]">
@@ -57,18 +73,50 @@ import { useState } from "react";
 
                     <div>
                         <p className="pb-4 text-white">Please select the image position</p>
-                        <select name="" id="" className="w-full h-[50px] p-2 bg-transparent border-[1px] rounded-md border-white text-white outline-none cursor-pointer">
-                            <option value="" className="h-[50px]">Front</option>
-                            <option value="">Back</option>
-                        </select>
+
+                        <Dropdown>
+                  <Dropdown.Toggle variant="success" id="dropdown-basic"
+                  className="flex flex-row justify-start items-center w-full">
+                   {position}
+                  </Dropdown.Toggle>
+
+                  <Dropdown.Menu>
+                     <Dropdown.Item href="#/action-1" onClick={()=>setPosition("Front")} >
+                        Front
+                     </Dropdown.Item>
+                  <Dropdown.Item href="#/action-2" onClick={()=>{setPosition("Back")}}>
+                        Back
+                     </Dropdown.Item>
+                  </Dropdown.Menu>
+            </Dropdown>
+                        
                         <br />
                         <p className="pt-6">Color</p>
-                        <div className="items-center justify-around">
-                            <button className="h-[40px] w-[80px] bg-blue-400 text-black" onClick={()=>setSelected(hodies.blue)}>Blue</button>
-                            <button className="h-[40px] w-[80px] bg-red-500 text-black" onClick={()=>setSelected(hodies.red)}>Red</button>
-                            <button className="h-[40px] w-[80px] bg-green-500 text-black" onClick={()=>setSelected(hodies.green)}>Green</button>
-                            <button className="h-[40px] w-[80px] bg-gray-500 text-black" onClick={()=>setSelected(hodies.grey)}>Gray</button>
-                            <button className="h-[40px] w-[80px] bg-white text-black" onClick={()=>setSelected(hodies.white)}>White</button>
+                        <div className="items-center justify-around">   
+                        <Dropdown>
+                  <Dropdown.Toggle variant="success" id="dropdown-basic"
+                  className="flex flex-row justify-start items-center w-full">
+                   {hodie}
+                  </Dropdown.Toggle>
+
+                  <Dropdown.Menu>
+                     <Dropdown.Item href="#/action-1" onClick={()=>setHodie("Blue Hoodie")} >
+                        Blue
+                     </Dropdown.Item>
+                  <Dropdown.Item href="#/action-2" onClick={()=>{setHodie("Red Hoodie")}}>
+                        Red
+                     </Dropdown.Item>
+                     <Dropdown.Item href="#/action-2" onClick={()=>{setHodie("Green Hoodie")}}>
+                        Green
+                     </Dropdown.Item>
+                     <Dropdown.Item href="#/action-2" onClick={()=>{setHodie("Gray hoodie")}}>
+                        Gray
+                     </Dropdown.Item>
+                     <Dropdown.Item href="#/action-2" onClick={()=>{setHodie("White hoodie")}}>
+                        White
+                     </Dropdown.Item>
+                  </Dropdown.Menu>
+            </Dropdown>
 
                         </div>
 
@@ -76,18 +124,34 @@ import { useState } from "react";
 
                             <div className="w-full">
                             <p>Size</p>
-                            <select name="" id="" className="w-[90%] bg-transparent text-center border-[1px] rounded-md border-white h-[50px]">
-                                <option value="">S</option>
-                            </select>
+                           
+                        <Dropdown>
+                  <Dropdown.Toggle variant="success" id="dropdown-basic"
+                  className="flex flex-row justify-start items-center text-center w-[90%]">
+                   {size}
+                  </Dropdown.Toggle>
+
+                  <Dropdown.Menu>
+                     <Dropdown.Item href="#/action-1" onClick={()=>setSize("S")} >
+                        S
+                     </Dropdown.Item>
+                  <Dropdown.Item href="#/action-2" onClick={()=>{setSize("M")}}>
+                        M
+                     </Dropdown.Item>
+                  </Dropdown.Menu>
+            </Dropdown>
                             </div>
                             
                             <div className="w-full">
                             <p>Quantity</p>
                             <select name="" id="" className="w-full text-center bg-transparent border-[1px] rounded-md
                              border-white h-[50px]">
-                                <option value="">1</option>
+                                <option value="" onClick={()=>setNumber(1)}>1</option>
+                                <option value="" onClick={()=>setNumber(2)}>2</option>
                             </select>
                             </div>
+
+                            
                         </div>
 
                         <div>
@@ -95,6 +159,7 @@ import { useState } from "react";
                         <p className="text-4xl font-bold">USD 20,00</p>
                         </div>
                     </div>
+                    <PaypalCheckoutButton product={{description: hodie, price: (price*number)}} />
                 </div>
             </div>
 
@@ -191,6 +256,8 @@ import { useState } from "react";
                         <div>
                         <h1 className="text-lg pt-10">Price</h1>
                         <p className="text-4xl font-bold">USD 20,00</p>
+
+                        <PaypalCheckoutButton product={{description: NavItem, price: PageItem}} />
                         </div>
                     </div>
 
@@ -229,11 +296,8 @@ import { useState } from "react";
 
                         <div className="flex flex-row justify-center place-items-center pt-10">
 
-                            <div className="w-full">
-                            <p>Size</p>
-                            <select name="" id="" className="w-[90%] bg-transparent text-center border-[1px] rounded-md border-white h-[50px]">
-                                <option value="">S</option>
-                            </select>
+                            <div className="w-full pb-2">
+                            <p>*Sizes are universal</p>
                             </div>
                             
                             <div className="w-full">
